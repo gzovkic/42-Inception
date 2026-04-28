b10dc61fa99a791ed18712834faec37254743bcc
gzovkic <gzovkic@vbox.42heilbronn.de>
2026-04-28 19:00:50 +0200

    fixed typo in from mysql to sql

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
 
     mysqld_safe &
     sleep 3
 
     mysql -u root <<EOF
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${MYSQL_ROOT_PASSWORD}');
+CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
+CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${SQL_ROOT_PASSWORD}');
 FLUSH PRIVILEGES;
 EOF
 
-    mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+    mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
 fi
 
 exec mysqld_safe
